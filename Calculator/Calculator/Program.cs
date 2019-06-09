03ac49674aaa1e8d2b47a2e44a14b9ccfd54fa5b
EvelinaAleksandrova <evety.1dtvd@gmail.com>
2019-06-09 16:54:22 +0300

    Calculator

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Title = "Calculator";
+            Console.WriteLine("Please enter your name.");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello,{name} this is your calculator!");
+
+            Console.WriteLine("Enter ('+' , '-' ,'*' ,'/')");
+            string symbol = Console.ReadLine();
+            Console.WriteLine("Enter your First number.");
+            double number1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your Second number.");
+            double number2 = double.Parse(Console.ReadLine());
+            double result = 0;
+
+            switch (symbol)
+            {
+                case "+":
+                    result = number1 + number2;
+                    break;
+                case " - ":
+                    result = number1 - number2;
+                    break;
+                case "*":
+                    result = number1 * number2;
+                    break;
+                case "/":
+                    result = number1 / number2;
+                    break;
+                default:
+                    Console.WriteLine("This is not correct format!");
+                    return;
+            }
+            Console.WriteLine($"Your result is: {result}");
+
+        }
+    }
+}
