944980af2bcbdaa75dfd90aeaabee96f9923c0be
EvelinaAleksandrova <evety.1dtvd@gmail.com>
2019-06-09 18:08:27 +0300

    Color pick up functionality.

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Calculator
 {
@@ -6,14 +7,44 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-
-
-            Console.BackgroundColor = ConsoleColor.Green;
             Console.Title = "Calculator";
+            Console.WriteLine("Enter a color");
+            string color = Console.ReadLine();
+            color = color.ToLower();
+
+            if (color == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+            }
+            else if (color == "green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+            }
+            else if (color == "magenta")
+            {
+                Console.BackgroundColor = ConsoleColor.Magenta;
+            }
+            else if (color == "yellow")
+            {
+                Console.BackgroundColor = ConsoleColor.Yellow;
+            }
+            else if (color == "white")
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+            }
+            else if (color == "blue")
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+            }
+            else
+            {
+                Console.WriteLine("The color can't set up.");
+            }
+
             Console.WriteLine("Please enter your name.");
             string name = Console.ReadLine();
             Console.WriteLine($"Hello,{name} this is your calculator!");
-
+            Thread.Sleep(2000);
             Console.WriteLine("Enter ('+' , '-' ,'*' ,'/')");
             string symbol = Console.ReadLine();
             Console.WriteLine("Enter your First number.");
